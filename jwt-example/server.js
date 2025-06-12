501a280a33c6fa2b3718fe7674702fa721e6f770
Josh Archer <jarcher@greenriver.edu>
2025-06-12 13:45:26 -0700

    Added missing router in server.js

--- jwt-example/server.js
+++ jwt-example/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import router from './routes/api.routes.js';
 import './config/jwt.config.js';
 
 const app = express();
 
 app.use(express.json());
+app.use("/", router);
 
 const PORT = 3000;
 
